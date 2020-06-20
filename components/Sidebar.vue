5f64426d08b41295751fc5ea46c6022c34da3055
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-06-20 11:41:00 -0400

    Add dummy sidebar

---
+++ components/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <aside>
+    Le Sidebar
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+aside {
+  min-width: 200px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
-    <PostPreview
-      v-for="post in posts"
-      :key="post.id"
-      :postid="post.id"
-      :title="post.title"
-      :text="post.content"
-    />
+    <section class="posts">
+      <PostPreview
+        v-for="post in posts"
+        :key="post.id"
+        :postid="post.id"
+        :title="post.title"
+        :text="post.content"
+      />
+    </section>
+    <Sidebar />
   </div>
 </template>
 
 <script>
 import PostPreview from '@/components/PostPreview';
+import Sidebar from '@/components/Sidebar';
 
 export default {
   components: {
     PostPreview,
+    Sidebar,
   },
   computed: {
     posts() {
@@ -25,4 +30,14 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+div {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+.posts {
+  min-width: 350px;
+}
+</style>
