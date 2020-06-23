ffc078cf3ce7450010fe5213149363d14ab80c8b
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-06-23 15:03:56 -0400

    Add favicons and icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,8 +18,41 @@ export default {
         name: 'description',
         content: process.env.npm_package_description || '',
       },
+      {
+        name: 'msapplication-TileColor',
+        content: '#da532c',
+      },
+      { name: 'theme-color', content: '#000000' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
   },
   /*
    ** Global CSS
